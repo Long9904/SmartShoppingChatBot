a14fc9854f37d454b3b04a348c4b37cecf42c00b
Long9904 <longvu09092004@gmail.com>
2026-06-23 15:28:14 +0700

    feat: URD for user

---
+++ SmartShoppingChatBot.API/Controllers/UsersController.cs
@@ -0,0 +1,80 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using SmartShoppingChatBot.Application.Commons.Results;
+using SmartShoppingChatBot.Application.DTOs;
+using SmartShoppingChatBot.Application.Features.UserManagement.DeleteUser;
+using SmartShoppingChatBot.Application.Features.UserManagement.GetAllUser;
+using SmartShoppingChatBot.Application.Features.UserManagement.GetUserById;
+using SmartShoppingChatBot.Application.Features.UserManagement.UpdateUser;
+using SmartShoppingChatBot.Domain.Commons;
+
+namespace SmartShoppingChatBot.API.Controllers
+{
+    [Route("api/v1/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [EndpointDescription("Get all users")]
+        [EndpointSummary("Get all users")]
+        [Authorize(Roles = "ADMIN")]
+        public async Task<IActionResult> GetAllUsers([FromQuery] GetAllUserQuery query)
+        {
+            var result = await _mediator.Send(query);
+            return StatusCode(result.StatusCode, ApiResponse<BasePaginatedList<object>>.Ok(result.Data!, result.Message));
+        }
+
+        [HttpGet("{id}")]
+        [EndpointDescription("Get user by ID")]
+        [EndpointSummary("Get user by ID")]
+        [Authorize(Roles = "ADMIN")]
+        public async Task<IActionResult> GetUserById([FromRoute] string id)
+        {
+            var result = await _mediator.Send(new GetUserByIdQuery { UserId = id });
+            if (result.IsSuccess)
+                return StatusCode(result.StatusCode, ApiResponse<object>.Ok(result.Data!, result.Message));
+            return StatusCode(result.StatusCode, ApiResponse<object>.Fail(result.Message!, result.Errors));
+        }
+
+        [HttpDelete("{id}")]
+        [EndpointDescription("Delete user by ID")]
+        [EndpointSummary("Delete user by ID")]
+        [Authorize(Roles = "ADMIN")]
+        public async Task<IActionResult> DeleteUserById([FromRoute] string id)
+        {
+            var result = await _mediator.Send(new DeleteUserCommand { UserId = id });
+            if (result.IsSuccess)
+                return StatusCode(result.StatusCode, ApiResponse<object>.Ok(null, result.Message));
+            return StatusCode(result.StatusCode, ApiResponse<object>.Fail(result.Message!, result.Errors));
+        }
+
+        [HttpPut("{id}")]
+        [EndpointDescription("Update user by ID")]
+        [EndpointSummary("Update user by ID")]
+        [Authorize(Roles = "ADMIN")]
+        public async Task<IActionResult> UpdateUserById([FromRoute] string id, [FromBody] UpdateUserCommand command)
+        {
+            if (!ObjectId.TryParse(id, out var userId))
+            {
+                return BadRequest(ApiResponse<ProfileResponse>.Fail("Invalid user ID."));
+            }
+
+            command.UserId = userId;
+
+            var result = await _mediator.Send(command);
+
+            if (result.IsSuccess)
+                return StatusCode(result.StatusCode, ApiResponse<object>.Ok(result.Data!, result.Message));
+            return StatusCode(result.StatusCode, ApiResponse<object>.Fail(result.Message!, result.Errors));
+        }
+    }
+}
---
+++ SmartShoppingChatBot.Application/Features/UserManagement/UpdateUser/UpdateUserCommandHandler.cs
@@ -0,0 +1,77 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using SmartShoppingChatBot.Application.Commons.Results;
+using SmartShoppingChatBot.Application.DTOs;
+using SmartShoppingChatBot.Application.Interface;
+using SmartShoppingChatBot.Domain.Interface;
+
+namespace SmartShoppingChatBot.Application.Features.UserManagement.UpdateUser
+{
+    public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, Result<ProfileResponse>>
+    {
+
+        private readonly IUserRepository _userRepository;
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly TimeProvider _time;
+        private readonly ILogger<UpdateUserCommandHandler> _logger;
+
+        public UpdateUserCommandHandler(
+            IUserRepository userRepository,
+            ICurrentUserService currentUserService,
+            IUnitOfWork unitOfWork,
+            IMapper mapper,
+            TimeProvider time,
+            ILogger<UpdateUserCommandHandler> logger)
+        {
+            _userRepository = userRepository;
+            _currentUserService = currentUserService;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _time = time;
+            _logger = logger;
+        }
+
+        public async Task<Result<ProfileResponse>> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+
+            var existingUser = await _userRepository.FindAsync(x => x.Id == request.UserId);
+
+            if (existingUser == null)
+            {
+                return Result<ProfileResponse>.Failure(404, "User not found");
+            }
+
+            var currentUserResult = await _currentUserService.GetUser();
+            if (!currentUserResult.IsSuccess)
+            {
+                return Result<ProfileResponse>.Failure(
+                    currentUserResult.StatusCode,
+                    currentUserResult.Message,
+                    currentUserResult.Errors);
+            }
+
+            var dateTimeNow = _time.GetUtcNow();
+
+            existingUser.FullName = request.FullName;
+            existingUser.PhoneNumber = request.PhoneNumber;
+            existingUser.DateOfBirth = request.DateOfBirth;
+            existingUser.Gender = request.Gender;
+            existingUser.UpdatedAt = dateTimeNow;
+            existingUser.UpdatedBy = new()
+            {
+                Id = currentUserResult.Data.Id,
+                Name = currentUserResult.Data.FullName
+            };
+
+            await _userRepository.UpdateAsync(existingUser);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            var profileResponse = _mapper.Map<ProfileResponse>(existingUser);
+
+            return Result<ProfileResponse>.Success(profileResponse, 200, "User updated successfully");
+        }
+    }
+}
